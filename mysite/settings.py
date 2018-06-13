a95bbe56714d8487551c2c21f5be2f9d19c48b14
jacob <jacob@david-MBP.local>
2018-06-13 15:30:22 +0800

    root

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': '995277',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
